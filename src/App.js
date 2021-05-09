c35132f14f2f8297b559c6e8d0dea16a423b0bda
Raj Priyam <1806406@kiit.ac.in>
2021-05-09 19:17:27 +0530

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import Profile from "./Profile";
+import  Profile_Editable  from "./Profile_Editable";
+import  Navbar  from "./Navbar";
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Profile_Manager from './Profile_Manager';
+
 
 function App() {
+  // return (
+  //   <div className="App">
+  //     <Profile/>
+  //   </div>
+  // );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Router>
+    //   <div className="App">
+        
+    //     <div className="content">
+    //       <Switch>
+    //         <Route exact path="/">
+    //           <Profile />
+    //         </Route>
+    //         <Route exact path="/edit">
+    //           <Profile_Editable />
+    //         </Route>
+    //       </Switch>
+    //     </div>
+    //   </div>
+    // </Router>
+    //<Profile_Manager account_email="abc@abc.com"/>
+    // <Router>
+    //   <div className="App">
+    //     <Navbar />
+    //     <div className="content">
+    //       <Switch>
+    //         <Route path="/">
+    //           <Profile_Manager />
+    //         </Route>
+    //       </Switch>
+    //     </div>
+    //   </div>
+    // </Router>
+
+    <>
+    <Navbar name="Raj"/>
+    <Profile_Manager account_email="abc@abc.com"/>
+    </>
   );
+
 }
 
 export default App;
